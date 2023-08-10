47980276e80acc84b4b7397abeb9a4ba5d828bed
BakiAbdullah <shatil605@gmail.com>
2023-08-11 00:28:23 +0600

    code refactored

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGray: "#B0A4A4",
+        pink: "#f3274c",
+      },
+      fontFamily: {
+        Fredoka: ["Fredoka", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

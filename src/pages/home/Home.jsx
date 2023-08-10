0f9c2999efbf776e8821928a6209ad05ec1854fb
DevReaxul <hreazul11@gmail.com>
2023-08-10 22:14:55 +0600

    initial cards done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../../components/Banner/Banner";
+import Cards from "../../sections/Cards";
+
 
 const Home = () => {
   return (
     <div>
       {/* Banner  */}
       <Banner />
+      <Cards></Cards>
     </div>
   );
 };

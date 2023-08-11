1f0fb1d1eb0df020561923fb07c0a16d06d3adec
DevReaxul <hreazul11@gmail.com>
2023-08-11 17:04:50 +0600

    useEffect added

--- src/sections/Cards.jsx
+++ src/sections/Cards.jsx
@@ -1,14 +1,20 @@
 // import foods from '../foods.json'
-import foods from '../../public/foods.json'
+// import foods from '../../public/foods.json'
+import { useEffect, useState } from 'react';
 import Card from '../components/Home/Card';
 const Cards = () => {
+    
+    const [featuredFood, setFeaturedFood] = useState([]);
+    useEffect(() => {
+        fetch('foods.json').then(res=>res.json().then(data=>setFeaturedFood(data)))
+    },[])
     
     return (
         <div className='font-Fredoka my-20 bg-white md:mx-24'>
             <p className='text-6xl font-bold text-center mb-20'>Featured Dishes</p>
             <div className='grid lg:grid-cols-3 gap-10'>
                 {
-                    foods.slice(0, 3).map((food,i) =><Card key={i} cardDetails={food}></Card>)
+                    featuredFood.slice(0, 3).map((food,i) =><Card key={i} cardDetails={food}></Card>)
                 }
             </div>
             

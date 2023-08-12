8764d82949c2acf7513da1101ed8f08213369ba2
DevReaxul <hreazul11@gmail.com>
2023-08-13 01:14:42 +0600

    category section added

--- src/components/ReserveTable/CategoryCard.jsx
+++ src/components/ReserveTable/CategoryCard.jsx
@@ -1,12 +1,12 @@
 
-const CategoryCard = ({ cardDetails }) => {
+const CategoryCard = ({ image, description, buttonText }) => {
     return (
       <div>
-        <img className="rounded-xl" src={cardDetails.image} alt="" />
-        <p >
-          {cardDetails.description}
+        <img className="rounded-3xl" src={image} alt="" />
+        <p className="my-5">
+          {description}
         </p>
-        <button>click here</button>
+        <button className="bg-pink rounded-3xl py-2 px-4 text-white font-medium hover:bg-darkPink">{buttonText}</button>
       </div>
     );
   };
---
+++ src/pages/home/userCategory/UserCategory.jsx
@@ -0,0 +1,19 @@
+import CategoryCard from "../../../components/ReserveTable/CategoryCard";
+import team from "../../../assets/asset/Category Card/team.jpg"
+import deliveryMan from "../../../assets/asset/Category Card/delivery-man.jpg"
+import restaurant from "../../../assets/asset/Category Card/restaurant (1).jpg"
+
+const UserCategory = () => {
+    return (
+        <div className="m-20">
+            <p className="text-5xl font-bold text-center text-black">user UserCategory</p>
+            <div className="grid md:grid-cols-3 gap-10">
+                <CategoryCard image={team} description={"A dynamic and inclusive workplace, where you will have the freedom to create your own journey. Find your next adventure in Tasty Drop."} buttonText={'Join the team!'}></CategoryCard>
+                <CategoryCard image={restaurant} description={"Grow your business while focusing on what you want to: making delicious food. Team up with Tasty Drop and tap into a fresh audience."} buttonText={"Become a partner!"}></CategoryCard>
+                <CategoryCard image={deliveryMan} description={"Join the rider community. We enable the freedom to pursue your dreams and passions, providing the flexibility and means to make it happen."} buttonText={"Become a rider!"}></CategoryCard>
+            </div>
+        </div>
+    );
+};
+
+export default UserCategory;

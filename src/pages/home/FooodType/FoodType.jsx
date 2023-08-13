7b1de31d577ee602321bee5a2fa266760f2c92fd
Naimur Reza <naimur.rezaa@gmail.com>
2023-08-13 18:33:02 +0600

    optimized food type part codes

--- src/pages/home/FooodType/FoodType.jsx
+++ src/pages/home/FooodType/FoodType.jsx
@@ -1,45 +1,42 @@
-import { useState } from "react";
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
-
 const FoodType = () => {
-
-    const [count, setCount] = useState(false);
-
-    return (
-        <ScrollTrigger onEnter={()=> setCount(true)} onExit={()=> setCount(false)}>
-            <div className="bg-slate-50 lg:flex justify-evenly py-12 text-center">
-            <div className="hover:bg-white p-10 rounded-md">
-                <p><i className="fa-solid fa-bowl-food text-6xl"></i></p>
-                <h3 className="text-3xl my-2 font-semibold">Menu Types</h3>
-                <p className="font-medium text-3xl text-orange-600">
-                    {count && <CountUp start={0} end={250} duration={3} delay={0}></CountUp>}
-                    +</p>
-            </div>
-            <div className="hover:bg-white p-10 rounded-md">
-                <span><i className="fa-solid fa-utensils text-6xl"></i></span>
-                <h3 className="text-3xl my-2 font-semibold">Different Origin</h3>
-                <p className="font-medium text-3xl text-orange-600">
-                    {count && <CountUp start={0} end={40} duration={3} delay={0}></CountUp>}
-                    +</p>
-            </div>
-            <div className="hover:bg-white p-10 rounded-md">
-                <span><i className="fa-solid fa-cookie text-6xl"></i></span>
-                <h3 className="text-3xl my-2 font-semibold">Special Dish</h3>
-                <p className="font-medium text-3xl text-orange-600">
-                    {count && <CountUp start={0} end={35} duration={3} delay={0}></CountUp>}
-                    +</p>
-            </div>
-            <div className="hover:bg-white p-10 rounded-md">
-                <span><i className="fa-solid fa-truck text-6xl"></i></span>
-                <h3 className="text-3xl my-2 font-semibold">Ready To Go</h3>
-                <p className="font-medium text-3xl text-orange-600">
-                    {count && <CountUp start={0} end={450} duration={3} delay={0}></CountUp>}
-                    +</p>
-            </div>
+  const Options = [
+    {
+      icon: "fa-bowl-food ",
+      text: "Menu Types",
+      count: 250,
+    },
+    {
+      icon: "fa-cookie",
+      text: "Different Origin",
+      count: 150,
+    },
+    {
+      icon: "fa-bowl-food",
+      text: "Special Dish",
+      count: 100,
+    },
+    {
+      icon: "fa-truck",
+      text: "Ready To Go",
+      count: 50,
+    },
+  ];
+  return (
+    <div className=" lg:flex justify-evenly py-12 text-center">
+      {Options.map((option) => (
+        <div className=" p-10 rounded-md bg-slate-50 lg:w-[300px] cursor-pointer">
+          <p>
+            <i className={`${option.icon} text-6xl fa-solid`}></i>
+          </p>
+          <h3 className="text-3xl my-2 font-semibold">{option.text}</h3>
+          <p className="font-medium text-3xl text-orange-600">
+            <CountUp enableScrollSpy={true} end={option.count} />+
+          </p>
         </div>
-        </ScrollTrigger>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default FoodType;

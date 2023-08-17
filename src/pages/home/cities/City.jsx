b765a1da1497b615a6229fa1bc3df0a1fbc485d3
Mdsakib999 <sakibabrar999@gmail.com>
2023-08-17 15:54:58 +0600

    some cng

--- src/pages/home/cities/City.jsx
+++ src/pages/home/cities/City.jsx
@@ -30,26 +30,6 @@ const cities = [
 ];
 
 const City = () => {
-<<<<<<< HEAD
-    return (
-      <section className=' md:mx-20 mt-10 md:mt-40'>
-            <MainHeading title={'Find us in these cities and many more!'}></MainHeading>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    {cities.map((city, index) => (
-        <Link to={`city/${city.name.toLowerCase()}`} key={index} className="relative group">
-        <img className="w-full h-40 md:h-60 object-cover rounded-lg transition-transform transform group-hover:scale-105" src={city.image} alt={city.name} />
-        <div className="absolute inset-0 flex items-center justify-center rounded-md">
-          <span className="absolute bottom-0 left-0 p-2 m-2 bg-white rounded-lg text-black text-lg font-semibold">{city.name}</span>
-        </div>
-      </Link>
-    ))}
-  </div>
-
-  {/* extar btn */}
-  <Link to="/restaurant">
-  <button className='bg-slate-300 p-2 rounded-lg'>restaurant</button>
-  </Link>
-=======
   return (
     <section className="md:mx-20 mt-10 md:mt-40">
       <MainHeading
@@ -73,7 +53,6 @@ const City = () => {
           </Link>
         ))}
       </div>
->>>>>>> 24a2a0dc43e4ffe093990630e2a8b17fc9af4564
     </section>
   );
 };

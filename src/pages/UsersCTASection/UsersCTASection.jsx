aa67a384c9c6d39f8c68affa6618b1c2b378dd96
BakiAbdullah <shatil605@gmail.com>
2023-08-18 16:38:05 +0600

    added users category section

---
+++ src/pages/UsersCTASection/UsersCTASection.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+
+const UsersCTASection = ({ctaImg, heading, subHeading, btnLabel}) => {
+  return (
+    <div className="w-[70%] flex mx-auto gap-20 items-center justify-center my-28">
+      <div>
+        <h5 className="text-pink text-3xl font-bold">{heading}!</h5>
+        <p className="text-black/70 py-3">{subHeading}</p>
+        <Link to="/">
+          <Button label={btnLabel}></Button>
+        </Link>
+      </div>
+      <div className="rounded-lg">
+        <img className="rounded-3xl " src={ctaImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default UsersCTASection;
--- src/pages/rider/Rider.jsx
+++ src/pages/rider/Rider.jsx
@@ -1,11 +1,11 @@
-
 import deliveryMan from "../../assets/asset/Category Card/delivery-man.jpg";
-import BannerTemplate from "../../components/DynamicBanner/BannerTemplate";
 import FacilityCard from "../../components/FacilityCard";
 import dealsImage from "../../assets/asset/facility-card-images/deals&discount.jpg"
 import earningImage from "../../assets/asset/facility-card-images/transparentEarning.jpg"
 import workImage from "../../assets/asset/facility-card-images/flexible-work.jpg"
 import gearImage from "../../assets/asset/facility-card-images/riderGear.jpg"
+import BannerTemplate from "../../components/dynamicBanner/BannerTemplate";
+import UsersCTASection from "../UsersCTASection/UsersCTASection";
 
 const Rider = () => {
  
@@ -26,6 +26,15 @@ const Rider = () => {
           "We enable the freedom to pursue your dreams and passions, providing the flexibility and means to make it happen. Whether you’re looking for a side gig or to achieve a personal dream, we’re here to help you achieve your goals at your own pace."
         }
       ></BannerTemplate>
+
+      <UsersCTASection
+        btnLabel={"Become a rider"}
+        heading={"Hello Rider"}
+        subHeading={
+          "Whether you’re looking for a side gig or to achieve a personal dream, we’re here to help you achieve your goals at your own pace.!"
+        }
+        ctaImg={gearImage}
+      ></UsersCTASection>
       <div>
         <p className="text-center text-3xl tracking-tighter text-sky-900 font-bold font-sans mt-20">
           {"LIFE'S ABOUT BALANCE, DECIDE WHEN TO WORK"}

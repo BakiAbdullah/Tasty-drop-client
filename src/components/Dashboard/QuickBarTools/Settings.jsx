7a87be0ff0763a0ce79732598658b2d62345888e
Naimur Reza <naimur.rezaa@gmail.com>
2023-08-22 21:32:41 +0600

    done with slide animation with css

--- src/components/Dashboard/QuickBarTools/Settings.jsx
+++ src/components/Dashboard/QuickBarTools/Settings.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const Settings = () => {
+export const Settings = ({ isShowSetting, setShowSetting }) => {
   return (
     <div
-      className={`absolute  right-[68px] top-[72px] bg-red-500 p-7 shadow-xl h-[300px] `}>
+      className={`${
+        isShowSetting ? "right-0" : ""
+      } absolute  right-[68px] top-[72px] bg-red-500 p-7 shadow-xl h-[300px] `}>
       <p>Dark Mode</p>
       <p>Hide Quick Bar</p>
     </div>

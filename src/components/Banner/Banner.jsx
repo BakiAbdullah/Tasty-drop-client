ff91d9d6d7c5b664a6b4f411754a5d8878f1052b
DevReaxul <hreazul11@gmail.com>
2023-08-22 21:54:45 +0600

    searchfield functionality added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,18 +1,28 @@
+import { useContext, useState } from "react";
 import banner from "../../assets/asset/Banner/Banner.jpg";
 import { motion } from "framer-motion";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Banner = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const { handleSearch } = useContext(AuthContext);
+  const handleSearchField = () => {
+    handleSearch( searchQuery );
+  };
+
   return (
     <div
       className="bg-cover bg-top min-h-[550px] relative"
-      style={{ backgroundImage: `url("${banner}")` }}>
+      style={{ backgroundImage: `url("${banner}")` }}
+    >
       <div className="absolute bg-black/40 inset-0 flex items-center justify-center ">
         <div>
           <motion.h1
             className="text-3xl md:text-6xl tracking-wide font-semibold text-center text-white"
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.2 }}>
+            transition={{ duration: 1, delay: 0.2 }}
+          >
             Satisfy your cravings with <br /> restaurant-quality{" "}
             <span className="text-orange-500">food</span>
           </motion.h1>
@@ -21,7 +31,8 @@ const Banner = () => {
             className=" relative md:w-[600px] mx-auto pt-5"
             initial={{ opacity: 0, y: 60 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.5, delay: 0.6 }}>
+            transition={{ duration: 1.5, delay: 0.6 }}
+          >
             <p className="text-center text-gray-200 text-white pt-3 text-base">
               Enter your desired food to see what we deliver to your area!
             </p>
@@ -30,8 +41,17 @@ const Banner = () => {
                 className="px-6 py-4 w-full mt-4 rounded-full text-lg"
                 type="text"
                 placeholder="Search for restaurants and cuisines"
+                onChange={(e) =>
+                  setSearchQuery(
+                    e.target.value.charAt(0).toUpperCase() +
+                      e.target.value.slice(1)
+                  )
+                }
               />
-              <button className="absolute right-2 btn-rounded top-[29%]">
+              <button
+                onClick={handleSearchField}
+                className="absolute right-2 btn-rounded top-[29%]"
+              >
                 Search
               </button>
             </div>

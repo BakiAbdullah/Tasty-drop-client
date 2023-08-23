9566b6677e5ab7f73ce53d98c2aa592d59931ed8
Naimur Reza <naimur.rezaa@gmail.com>
2023-08-23 10:21:14 +0600

    added updated code

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Toggle = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <label className="relative inline-flex items-center mr-5 cursor-pointer">
-      <input type="checkbox" value="" className="sr-only peer" checked />
-      <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
-      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-        Dark Mode
-      </span>
-    </label>
+    <>
+      <label className="flex cursor-pointer select-none items-center">
+        <div className="relative">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className="sr-only"
+          />
+          <div
+            className={`box block h-8 w-14 rounded-full ${
+              isChecked ? "bg-primary" : "bg-dark"
+            }`}></div>
+          <div
+            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition ${
+              isChecked ? "translate-x-full" : ""
+            }`}></div>
+        </div>
+      </label>
+    </>
   );
 };
 

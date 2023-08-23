ee8b15ab103d5b30b271e1feda608f03fd5de37b
Naimur Reza <naimur.rezaa@gmail.com>
2023-08-23 18:54:50 +0600

    Added setting menu using headless ui toggle

--- src/components/Dashboard/QuickBarTools/Settings/Settings.jsx
+++ src/components/Dashboard/QuickBarTools/Settings/Settings.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import "./Settings.css"; // Import your custom CSS file
 import Toggle from "../../../Toggle";
-
+import { RxCrossCircled } from "react-icons/rx";
 export const Settings = ({ isShowSetting, setShowSetting }) => {
   return (
     <div
       className={`settings-panel ${
         isShowSetting ? "visible" : "invisible"
       } bg-white border border-slate-200 p-7 shadow-xl space-y-4 text-lg`}>
-      <Toggle />
-      <Toggle />
+      <span className="flex items-center  justify-between">
+        <h1 className="text-slate-500">Settings</h1>
+        <button onClick={() => setShowSetting(!isShowSetting)}>
+          <RxCrossCircled className="hover:text-slate-500 " size={22} />
+        </button>
+      </span>
+      <hr className="border border-slate-200" />
+      <h1 className="lg:text-[15px] font-semibold text-slate-500">Customize</h1>
+      <p className="flex items-center gap-3 text-sm lg:text-[15px] ">
+        <Toggle /> Dark Mode
+      </p>
+      <p className="flex items-center gap-3 text-sm lg:text-[15px] ">
+        <Toggle /> Hide Quickbar
+      </p>
+
+      <h1 className="lg:text-[15px] font-semibold text-slate-500">
+        Keyboard shortcuts
+      </h1>
+      <p className="text-sm">
+        <span className="text-orange-500 font-mono font-semibold">Esc</span>{" "}
+        close quickbar
+      </p>
+      <p className="text-sm">
+        <span className="text-orange-500 font-mono font-semibold">
+          Alt + (1 - 6)
+        </span>{" "}
+        to open quickbar
+      </p>
     </div>
   );
 };
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,34 +1,23 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
-const Toggle = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
+export default function Toggle() {
+  const [enabled, setEnabled] = useState(false);
 
   return (
-    <>
-      <label className="flex cursor-pointer select-none items-center">
-        <div className="relative">
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={handleCheckboxChange}
-            className="sr-only"
-          />
-          <div
-            className={`box block h-8 w-14 rounded-full ${
-              isChecked ? "bg-primary" : "bg-dark"
-            }`}></div>
-          <div
-            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition ${
-              isChecked ? "translate-x-full" : ""
-            }`}></div>
-        </div>
-      </label>
-    </>
+    <div>
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? "bg-black" : "bg-orange-500"}
+          relative inline-flex h-[18px] w-[38px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}>
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          className={`${enabled ? "translate-x-5" : "translate-x-0"}
+            pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+        />
+      </Switch>
+    </div>
   );
-};
-
-export default Toggle;
+}

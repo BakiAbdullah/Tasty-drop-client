efe311ae2093f5961e58faa91531af275933aeb0
DevReaxul <hreazul11@gmail.com>
2023-08-24 20:37:31 +0600

    bug fixed from search field and enter key functionality added

--- src/pages/home/SearchResult/SearchResultSection.jsx
+++ src/pages/home/SearchResult/SearchResultSection.jsx
@@ -1,29 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import RestaurantCard from "../../../components/Cards/RestaurantCard";
+import Loader from "../../../components/Loader/Loader";
 
 const SearchResultSection = () => {
   const { searchResults } = useContext(AuthContext);
-  console.log(searchResults);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (searchResults) {
+      setLoading(false);
+    }
+  }, [searchResults]);
+
   return (
     <section className="content">
-      {searchResults?.length ? (
-        <>
-          <p className="text-4xl mx-40 my-8">Search results</p>
-          <div className="mx-40 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-            {searchResults.map((restaurant) => (
-              <RestaurantCard
-                key={restaurant._id}
-                restaurant={restaurant}></RestaurantCard>
-            ))}
+    {loading ? (
+      <Loader /> // Show loader when data is loading from the api....
+    ) : (
+      <>
+        {searchResults?.length ? (
+          <>
+            <p className="text-4xl mx-40 my-8">Search results</p>
+            <div className="mx-40 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+              {searchResults.map((restaurant) => (
+                <RestaurantCard
+                  key={restaurant._id}
+                  restaurant={restaurant}
+                ></RestaurantCard>
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className="text-center font-semibold text-5xl my-40">
+            No search results found.
           </div>
-        </>
-      ) : (
-        <div className="text-center font-semibold text-5xl my-40">
-          No search results found.
-        </div>
-      )}
-    </section>
+        )}
+      </>
+    )}
+  </section>
   );
 };
 

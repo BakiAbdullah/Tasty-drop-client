35fe7101e58a770eb423d4e6c7efa742e2c98272
BakiAbdullah <shatil605@gmail.com>
2023-08-26 23:29:50 +0600

    api for search and restaurant data route customized

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,9 @@
 import SponsorGallery from "./SponsorGallery/SponsorGallery";
-// import ReserveTable from "../../components/ReserveTable/ReserveTable";
-// import Reviews from "../../components/Reviews/Reviews";
-// import FoodType from "./FoodType/FoodType";
 import UserCategory from "./userCategory/UserCategory";
 import DailyDeals from "../../components/shared/DailyDealsCard/DailyDeals";
 import City from "./cities/City";
 //messenger app
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import SearchResultSection from "./SearchResult/SearchResultSection";
@@ -25,10 +22,10 @@ const Home = () => {
           <UserCategory></UserCategory>
           <City></City>
           <DailyDeals></DailyDeals>
-          <MessengerCustomerChat
+          {/* <MessengerCustomerChat
             pageId="1875434189178634"
             appId="240131418459493"
-          />
+          /> */}
         </>
       )}
     </>

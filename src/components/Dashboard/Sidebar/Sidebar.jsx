ccaae046b6e1ef4ef57a060f6c4192a784327c98
BakiAbdullah <shatil605@gmail.com>
2023-08-28 00:19:36 +0600

    merged changed

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -4,7 +4,6 @@ import {
   adminOptions,
   riderOptions,
   partnerOptions,
-  businessOptions,
   customerOptions,
 } from "../../../constant/SideBarOptions";
 import { Profile } from "../Profile/Profile";

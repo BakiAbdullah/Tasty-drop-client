07edb1e2bec99fbb94e61e0df5dc2a08e6609b68
BakiAbdullah <shatil605@gmail.com>
2023-08-29 23:07:40 +0600

    user role not fixed

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -10,7 +10,7 @@ const store = configureStore({
   },
   middleware: getDefaultMiddleware =>{
     const issue = getDefaultMiddleware({
-      serializableCheck: false  //comment out this line and checkout browser console 
+      // serializableCheck: false  //comment out this line and checkout browser console 
     })
     const roleApisMiddleware  = roleApis.middleware
     // const roleApisMiddleware  = getDefaultMiddleware().concat(roleApis.middleware)

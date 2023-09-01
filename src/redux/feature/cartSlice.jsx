bf21f55ae945f0787dd1283a011fbdc469c0b1ca
BakiAbdullah <shatil605@gmail.com>
2023-09-01 11:51:27 +0600

    restructured some code

--- src/redux/feature/cartSlice.jsx
+++ src/redux/feature/cartSlice.jsx
@@ -1,26 +1,24 @@
-
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    carts: []
-}
+  carts: [],
+};
 
 export const cartSlice = createSlice({
-    name:'carts',
-    initialState,
-    reducers:{
-        addToCart:(state,{payload})=>{
-            const isExist = state.carts.find(item=>item._id === payload._id )
-            if(isExist){
-                isExist.quantity++
-                isExist.menuItemPrice = isExist.quantity * isExist.menuItemPrice
-            }
-            else{
-                state.carts.push({...payload,quantity:1})
-            }
-        }
-    }
-})
+  name: "carts",
+  initialState,
+  reducers: {
+    addToCart: (state, { payload }) => {
+      const isExist = state.carts.find((item) => item._id === payload._id);
+      if (isExist) {
+        isExist.quantity++;
+        isExist.menuItemPrice = isExist.quantity * isExist.menuItemPrice;
+      } else {
+        state.carts.push({ ...payload, quantity: 1 });
+      }
+    },
+  },
+});
 
-export const {addToCart} = cartSlice.actions
-export default cartSlice.reducer
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;

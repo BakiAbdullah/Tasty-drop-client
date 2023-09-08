7405c32253edb3555b3f6c4075175141a5440abf
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-08 18:50:20 +0600

    add: Empty order state with global component

--- src/pages/orderHistory/OrderHistory.jsx
+++ src/pages/orderHistory/OrderHistory.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import EmptyState from "../../components/Utils/EmptyState";
+import plateIcon from "../../assets/icon/plate.svg";
 export const OrderHistory = () => {
-  return <div>OrderHistory</div>;
+  const orders = [];
+  return (
+    <>
+      {orders && Array.isArray(orders) && orders.length > 0 ? (
+        ""
+      ) : (
+        <EmptyState
+          text={"No orders"}
+          imageSrc={plateIcon}
+          message={"You don't have any orders in your history."}
+        />
+      )}
+    </>
+  );
 };

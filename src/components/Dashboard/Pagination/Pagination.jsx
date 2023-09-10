53ec27714a4536a4607de3afe52bc8e0fccca956
DevReaxul <hreazul11@gmail.com>
2023-09-10 20:39:55 +0600

    pagination component added

---
+++ src/components/Dashboard/Pagination/Pagination.jsx
@@ -0,0 +1,25 @@
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="mt-4 flex justify-center">
+      <ul className="flex space-x-2">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <li key={index}>
+            <button
+              onClick={() => onPageChange(index + 1)}
+              className={`px-3 py-1 rounded-md ${
+                currentPage === index + 1
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 text-gray-700"
+              }`}
+            >
+              {index + 1}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;

5ce191e9495806a5ec5898e320c0422ab9a1ae26
DevReaxul <hreazul11@gmail.com>
2023-09-11 03:46:02 +0600

    working on backend fornend and ui

--- src/pages/Dashboard/Rider/ManageOrders.jsx
+++ src/pages/Dashboard/Rider/ManageOrders.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import Pagination from "../../../components/Dashboard/Pagination/Pagination";
@@ -6,21 +6,24 @@ import Pagination from "../../../components/Dashboard/Pagination/Pagination";
 export const ManageOrders = () => {
   const [orders, setOrders] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [ordersPerPage] = useState(6); // Number of orders to display per page
+  const ordersPerPage = 6; // Number of orders to display per page
   const [loading, setLoading] = useState(true);
 
+  const fetchOrders = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_LIVE_URL}api/orders`
+      );
+      setOrders(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    // Fetch orders from your backend API
-    axios
-      .get(`${import.meta.env.VITE_LIVE_URL}api/orders`)
-      .then((response) => {
-        setOrders(response.data); // Set the fetched orders in the state
-        setLoading(false); // Update loading status
-      })
-      .catch((error) => {
-        console.error("Error fetching orders:", error);
-        setLoading(false);
-      });
+    fetchOrders();
   }, []);
 
   const indexOfLastOrder = currentPage * ordersPerPage;
@@ -29,56 +32,39 @@ export const ManageOrders = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const handleAcceptOrder = (orderId) => {
-    console.log(orderId);
-    axios
-      .put(`${import.meta.env.VITE_LIVE_URL}api/orders/accept/${orderId}`)
-      .then((response) => {
-        // Handle success, show a success toast
-        toast.success(response.data.message);
-        // Refresh the orders list
-        fetchOrders();
-      })
-      .catch((error) => {
-        // Handle error, show an error toast
-        toast.error("Error accepting order");
-        console.error("Error accepting order:", error);
-      });
+  const handleAcceptOrder = async (orderId) => {
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_LIVE_URL}api/orders/accept/${orderId}`
+      );
+      toast.success(response.data.message);
+      fetchOrders();
+    } catch (error) {
+      toast.error("Error accepting order");
+      console.error("Error accepting order:", error);
+    }
   };
 
-  const handleDeclineOrder = (orderId) => {
-    axios
-      .put(`${import.meta.env.VITE_LIVE_URL}api/orders/decline/${orderId}`)
-      .then((response) => {
-        // Handle success, show a success toast
-        toast.success(response.data.message);
-        // Refresh the orders list
-        fetchOrders();
-      })
-      .catch((error) => {
-        // Handle error, show an error toast
-        toast.error("Error declining order");
-        console.error("Error declining order:", error);
-      });
-  };
-
-  const fetchOrders = () => {
-    // Fetch orders from your backend API
-    axios
-      .get(`${import.meta.env.VITE_LIVE_URL}api/orders`)
-      .then((response) => {
-        setOrders(response.data); // Set the fetched orders in the state
-        setLoading(false); // Update loading status
-      })
-      .catch((error) => {
-        console.error("Error fetching orders:", error);
-        setLoading(false);
-      });
+  const handleDeclineOrder = async (orderId) => {
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_LIVE_URL}api/orders/decline/${orderId}`
+      );
+      toast.success(response.data.message);
+      fetchOrders();
+    } catch (error) {
+      toast.error("Error declining order");
+      console.error("Error declining order:", error);
+    }
   };
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
+  const thClass =
+    "px-6 py-3 text-left text-xs text-slate-600 uppercase tracking-wider";
+  const tdClass = "px-6 py-4 whitespace-nowrap text-sm text-gray-900";
 
   return (
     <div className="bg-gray-100 min-h-screen p-6">
@@ -87,64 +73,33 @@ export const ManageOrders = () => {
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Order ID
-              </th>
-              {/* Add more table headers based on your data */}
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Customer
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Items
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Total
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Status
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Actions
-              </th>
+              <th className={thClass}>Order Info</th>
+              <th className={thClass}>Items</th>
+              <th className={thClass}>Total</th>
+              <th className={thClass}>Status</th>
+              <th className={thClass}>Actions</th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200">
-            {currentOrders.map((order) => (
+            {currentOrders?.map((order) => (
               <tr key={order._id}>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {order._id}
+                <td className={tdClass}>
+                  <div className="">
+                  {order.customerData.name}</div> 
+                    
+                 <span className="text-xs">
+                  {`${order.homeAddress.area}, ${order.homeAddress.upazila}, ${order.homeAddress.district}`}
+                  </span>
                 </td>
-                {/* Render more order details based on your data */}
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {order.customerData.name}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {/* Render the list of items here */}
+
+                <td className={tdClass}>
                   {order.foodArray.map((item) => (
                     <div key={item.productTotalPrice}>
                       {item.productTotalPrice}
                     </div>
                   ))}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{`$${order.totalPrice}`}</td>
+                <td className={tdClass}>{`$${order.totalPrice}`}</td>
                 <td className="px-6 py-4 whitespace-nowrap">
                   <span
                     className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
@@ -154,34 +109,33 @@ export const ManageOrders = () => {
                     }`}
                   >
                     {order.paymentStatus === true ? "Paid" : "Unpaid"}
+                  </span>{" "}
+                  &nbsp;
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100">
+                    {order.delivery}
                   </span>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
-                  {order.paymentStatus === true ? (
-                    <div className="flex space-x-2">
-                      <button
-                        className="px-2 py-1 bg-green-500 text-white rounded-md hover:bg-green-600"
-                        onClick={() => handleAcceptOrder(order._id)}
-                      >
-                        Accept
-                      </button>
-                      <button
-                        className="px-2 py-1 bg-pink text-white rounded-md hover:bg-red-600"
-                        onClick={() => handleDeclineOrder(order._id)}
-                      >
-                        Decline
-                      </button>
-                    </div>
-                  ) : (
-                    <span className="text-red-600">Unpaid</span>
-                  )}
+                  <div className="flex space-x-2">
+                    <button
+                      className="px-2 py-1 bg-green-500 text-white rounded-md hover:bg-green-600"
+                      onClick={() => handleAcceptOrder(order._id)}
+                    >
+                      Accept
+                    </button>
+                    <button
+                      className="px-2 py-1 bg-pink text-white rounded-md hover:bg-red-600"
+                      onClick={() => handleDeclineOrder(order._id)}
+                    >
+                      Decline
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      {/* Pagination */}
       <Pagination
         currentPage={currentPage}
         totalPages={Math.ceil(orders.length / ordersPerPage)}
@@ -190,3 +144,5 @@ export const ManageOrders = () => {
     </div>
   );
 };
+
+export default ManageOrders;

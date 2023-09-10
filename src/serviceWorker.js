f9ce3d5512d1b1cf300b8adf3ead6608a6674220
DevReaxul <hreazul11@gmail.com>
2023-09-10 17:30:07 +0600

    push notification added

---
+++ src/serviceWorker.js
@@ -0,0 +1,10 @@
+self.addEventListener('push', (event) => {
+    const options = {
+        body: event.data.text(),
+        icon: './logo.png', //our tasty drop logo
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification Title', options)
+    );
+});

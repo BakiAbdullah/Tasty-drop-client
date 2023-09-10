6e4a19fab78dbde5f8402c45e32f414bcdba96b7
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-10 21:27:46 +0600

    rightbar issue fixed

--- src/components/Utils/RightBar.jsx
+++ src/components/Utils/RightBar.jsx
@@ -29,12 +29,14 @@ export const RightBar = ({ showRightBar, setShowRightBar }) => {
       <hr className="text-slate-300" />
       <div className="m-5">
         {user ? (
-          <button
-            onClick={() => logOut()}
-            className="text-base md:text-lg btn-primary inline-flex items-center gap-2  absolute bottom-2 right-0">
-            <AiFillHome size={18} />
-            <Link>logout</Link>
-          </button>
+          <>
+            <button
+              onClick={() => logOut()}
+              className="text-base md:text-lg btn-primary inline-flex items-center gap-2  absolute bottom-2 right-0">
+              <AiFillHome size={18} />
+              <Link>logout</Link>
+            </button>
+          </>
         ) : (
           <button
             onClick={() => setShowRightBar(!showRightBar)}
@@ -43,21 +45,21 @@ export const RightBar = ({ showRightBar, setShowRightBar }) => {
             <Link to="/loginpage">Sign up or Log in</Link>
           </button>
         )}
-        {userRole !== "customer" && typeof userRole !== "undefined" && (
-          <Link to={`/dashboard/${userRole}`} className="w-full">
-            <button
-              onClick={() => setShowRightBar(!showRightBar)}
-              className="btn-bar text-left">
-              <RxDashboard size={20} className="text-zinc-500" />
-
-              <p className="inline-flex flex-col text-[15px] hover:text-orange-500 transition-all ">
-                Dashboard
-              </p>
-            </button>
-          </Link>
-        )}
         {user && (
           <div className="flex flex-col items-start  gap-2 mt-3">
+            {userRole !== "customer" && typeof userRole !== "undefined" && (
+              <Link to={`/dashboard/${userRole}`} className="w-full">
+                <button
+                  onClick={() => setShowRightBar(!showRightBar)}
+                  className="btn-bar text-left">
+                  <RxDashboard size={20} className="text-zinc-500" />
+
+                  <p className="inline-flex flex-col text-[15px] hover:text-orange-500 transition-all ">
+                    Dashboard
+                  </p>
+                </button>
+              </Link>
+            )}
             <Link to="/profile" className="w-full">
               <button
                 onClick={() => setShowRightBar(!showRightBar)}

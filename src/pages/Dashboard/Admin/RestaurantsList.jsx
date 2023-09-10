998dd9a7a6234710a25f646dfeafbdeafeab0002
imransarkerweb <imransarker.web@gmail.com>
2023-09-10 23:34:10 +0600

    restaurant list eye icon activated

--- src/pages/Dashboard/Admin/RestaurantsList.jsx
+++ src/pages/Dashboard/Admin/RestaurantsList.jsx
@@ -5,11 +5,20 @@ import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
 export const RestaurantsList = () => {
   const cellAlignClass = "px-4 py-2 text-center align-middle"; // Store the common class in a variable
   const [restaurants, setRestaurants] = useState([]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false); // New state for modal visibility
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null); // Store the selected restaurant
   useEffect(() => {
     fetch(`${import.meta.env.VITE_LIVE_URL}restaurants`)
       .then((res) => res.json())
       .then((data) => setRestaurants(data));
   }, []);
+
+  // Function to toggle the modal and set the selected restaurant
+  const toggleModal = (restaurant) => {
+    setSelectedRestaurant(restaurant);
+    setIsModalOpen(!isModalOpen);
+  };
 
   console.log(restaurants);
   return (
@@ -81,7 +90,12 @@ export const RestaurantsList = () => {
                   <td
                     className={`${cellAlignClass}} flex gap-2 justify-center items-center h-32`}
                   >
-                    <AiOutlineEye className="text-blue-500 hover:text-blue-700 cursor-pointer" />
+                    <span
+                      onClick={() => toggleModal(restaurant)} // Pass the restaurant data to toggleModal
+                      className="text-blue-500 hover:text-blue-700 cursor-pointer"
+                    >
+                      <AiOutlineEye />
+                    </span>
                     <AiOutlineDelete className="text-red-500 hover:text-red-700 cursor-pointer" />
                   </td>
                   <td className={cellAlignClass}></td>
@@ -90,6 +104,59 @@ export const RestaurantsList = () => {
             })}
         </tbody>
       </table>
+      {/* modal */}
+      {isModalOpen && selectedRestaurant && (
+        <div className="fixed inset-0 flex justify-center items-center z-50 bg-gray-800 bg-opacity-50 transition-opacity duration-300">
+          <div className="bg-white p-8 rounded-lg shadow-lg w-4/5 md:w-3/5 lg:w-2/5">
+            <h2 className="text-xl font-bold mb-4">
+              {selectedRestaurant.outletName}
+            </h2>
+            <img
+              src={selectedRestaurant.photo}
+              alt="Restaurant Photo"
+              className="w-32 h-32 mb-4 rounded-lg"
+            />
+            <p className="mb-2 text-gray-600">
+              Category: {selectedRestaurant?.RestaurantCategory}
+            </p>
+            <p className="mb-2 text-gray-600">
+              Contact: {selectedRestaurant.contactNumber}
+            </p>
+            <p className="mb-2 text-gray-600">
+              Discount on Items: {selectedRestaurant.discountOnItems}%
+            </p>
+            <p className="mb-2 text-gray-600">
+              Email: {selectedRestaurant.email}
+            </p>
+            <p className="mb-2 text-gray-600">
+              Owner: {selectedRestaurant.firstName}{" "}
+              {selectedRestaurant.lastName}
+            </p>
+            <p className="mb-2 text-gray-600">
+              Location: {selectedRestaurant.locations?.division},{" "}
+              {selectedRestaurant.locations?.district},{" "}
+              {selectedRestaurant.locations?.upazila}
+            </p>
+            <p className="mb-2 text-gray-600">
+              Total Menu Items: {selectedRestaurant.menu?.length}
+            </p>
+            <div className="text-sm mt-2">
+              <ReactStarsRating
+                className="flex"
+                isEdit={false}
+                size={24}
+                value={4}
+              />
+            </div>
+            <button
+              onClick={toggleModal}
+              className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-700 transition-colors duration-300"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

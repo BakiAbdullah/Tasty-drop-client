7526bea8bea0bdcb9cf0fd5c4456e818d13fecd8
imransarkerweb <imransarker.web@gmail.com>
2023-09-11 18:31:04 +0600

    stripe installed

--- src/components/Modal/CheckoutModal.jsx
+++ src/components/Modal/CheckoutModal.jsx
@@ -16,7 +16,8 @@ const CheckoutModal = ({ closeModal, isOpen, subscription, refetch }) => {
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0">
+          leaveTo="opacity-0"
+        >
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
@@ -29,7 +30,8 @@ const CheckoutModal = ({ closeModal, isOpen, subscription, refetch }) => {
               enterTo="opacity-100 scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95">
+              leaveTo="opacity-0 scale-95"
+            >
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Elements stripe={stripePromise}>
                   <CheckoutForm

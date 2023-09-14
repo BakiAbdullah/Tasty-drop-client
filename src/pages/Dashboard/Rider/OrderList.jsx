9a3a6b7664a95e558724e7358e49d7dfb0c8240e
DevReaxul <hreazul11@gmail.com>
2023-09-14 18:40:20 +0600

    fixing bug from dashboard

--- src/pages/Dashboard/Rider/OrderList.jsx
+++ src/pages/Dashboard/Rider/OrderList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   faTimesCircle,
@@ -42,76 +42,96 @@ const OrderList = () => {
   };
 
   return (
-    orders.length?
-    <div className="container mx-auto py-4">
-      {selectedOrder && (
-        <div
-          className={`${
-            showModal ? "fixed" : "hidden"
-          } inset-0 overflow-y-auto flex items-center justify-center z-50`}
+   orders.length? <div className="container mx-auto py-4">
+    {selectedOrder && (
+  <div
+    className={`${
+      showModal ? "fixed" : "hidden"
+    } inset-0 overflow-y-auto flex items-center justify-center z-50`}
+  >
+    <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
+    <div className="bg-white w-full md:w-3/4 lg:w-2/3 xl:w-1/2 rounded-lg p-6 z-10 shadow-lg">
+      <div className="flex items-center justify-between mb-6">
+        <h2 className="text-3xl font-semibold text-blue-600">
+          <FontAwesomeIcon icon={faFileAlt} className="mr-2" />
+          Order Details
+        </h2>
+        <button
+          onClick={closeModal}
+          className="text-gray-600 hover:text-gray-800 focus:outline-none"
         >
-          <div className="absolute inset-0 bg-gray opacity-75"></div>
-          <div className="bg-white w-full md:w-1/2 rounded-lg p-6 z-10 shadow-lg">
-            <div className="flex items-center justify-between">
-              <h2 className="text-2xl font-semibold text-blue-600 mb-4">
-                <FontAwesomeIcon icon={faFileAlt} className="mr-2" />
-                Order Details
-              </h2>
-              <button
-                onClick={closeModal}
-                className="hover:text-gray-800 focus:outline-none"
+          <FontAwesomeIcon icon={faTimesCircle} size="lg" />
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <h3 className="text-xl font-semibold text-gray-800">
+            <FontAwesomeIcon icon={faUser} className="mr-2" />
+            Customer Information
+          </h3>
+          <p className="text-gray-700 mt-3">
+            <span className="font-semibold">Name:</span>{" "}
+            {selectedOrder.customerData.name}
+          </p>
+          <p className="text-gray-700 mt-3">
+            <span className="font-semibold">Full Address:</span>{" "}
+            {`${selectedOrder.homeAddress.area}, ${selectedOrder.homeAddress.upazila}, ${selectedOrder.homeAddress.district}, ${selectedOrder.homeAddress.division}`}
+          </p>
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold text-gray-800">
+            <FontAwesomeIcon icon={faClipboard} className="mr-2" />
+            Order Summary
+          </h3>
+          <ul className="text-gray-700 mt-3">
+            {selectedOrder.orderInfo.map((item, index) => (
+              <li
+                key={index}
+                className="flex justify-between items-center mb-2"
               >
-                <FontAwesomeIcon icon={faTimesCircle} />
-              </button>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                  <FontAwesomeIcon icon={faUser} className="mr-2" />
-                  Customer Information
-                </h3>
-                <p className="text-gray-700 mb-2">
-                  <span className="font-semibold">Name:</span>{" "}
-                  {selectedOrder.customerData.name}
-                </p>
-                <p className="text-gray-700 mb-2">
-                  <span className="font-semibold">Full Address:</span>{" "}
-                  {`${selectedOrder.homeAddress.area}, ${selectedOrder.homeAddress.upazila},${selectedOrder.homeAddress.district},${selectedOrder.homeAddress.division}`}
-                </p>
-              </div>
-              <div>
-                <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                  <FontAwesomeIcon icon={faClipboard} className="mr-2" />
-                  Order Summary
-                </h3>
-                <p className="text-gray-700 mb-2">
-                  <span className="font-semibold">Items:</span>{" "}
-                  {selectedOrder.foodArray
-                    .map((item) => item.productTotalPrice)
-                    .join(", ")}
-                </p>
-                <p className="text-gray-700 mb-2">
-                  <span className="font-semibold">Total:</span> $
-                  {selectedOrder.totalPrice.toFixed(2)}
-                </p>
-                <p className="text-gray-700 mb-2">
-                  <span className="font-semibold">Payment Status:</span>{" "}
-                  {selectedOrder.paymentStatus ? "Paid" : "Unpaid"}
-                </p>
-                <div className="flex items-center">
-                  <span className="text-sm text-gray-700">
-                    Received by Rider{" "}
-                    <FontAwesomeIcon
-                      icon={faTruck}
-                      className="text-blue-600 ml-1"
-                    />
-                  </span>
-                </div>
-              </div>
-            </div>
+                <span>{item.itemName}</span>
+                <span className="font-semibold">
+                  ${item.productTotalPrice.toFixed(2)}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <hr className="my-4 border-gray-300" />
+          <div className="flex justify-between items-center">
+            <span className="text-gray-700 font-semibold">Total:</span>
+            <span className="text-2xl text-blue-600">
+              ${selectedOrder.orderInfo
+                .reduce((total, item) => total + item.productTotalPrice, 0)
+                .toFixed(2)}
+            </span>
+          </div>
+          <div className="mt-3">
+            <span className="text-gray-700">
+              Payment Status:{" "}
+              <span
+                className={`${
+                  selectedOrder.paymentStatus
+                    ? "text-green-600"
+                    : "text-red-600"
+                } font-semibold`}
+              >
+                {selectedOrder.paymentStatus ? "Paid" : "Unpaid"}
+              </span>
+            </span>
+          </div>
+          <div className="flex items-center mt-4">
+            <span className="text-gray-700">
+              Received by Rider{" "}
+              <FontAwesomeIcon icon={faTruck} className="text-blue-600 ml-2" />
+            </span>
           </div>
         </div>
-      )}
+      </div>
+    </div>
+  </div>
+)}
+
+
 
       <div className="mb-6">
         <h2 className="text-2xl font-semibold text-green-600 mb-2">
@@ -156,11 +176,13 @@ const OrderList = () => {
           </tbody>
         </table>
       </div>
-    </div>:<EmptyState
-          text={"No order to delivered"}
-          imageSrc={plateIcon}
-          message={"You don't accepted any order for delivery."}
-        />
+    </div>
+   : 
+    <EmptyState
+      text={"No order to deliver"}
+      imageSrc={plateIcon}
+      message={"You don't have any accepted orders for delivery."}
+    />
   );
 };
 

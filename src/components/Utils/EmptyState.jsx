16848493e54f5a30919ae48acdd38a8c537997d5
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-18 15:46:57 +0600

    routes optimized

--- src/components/Utils/EmptyState.jsx
+++ src/components/Utils/EmptyState.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const EmptyState = ({ imageSrc, message, address, label, text }) => {
   return (
-    <div className="min-h-screen gap-3 flex flex-col justify-center items-center  ">
+    <div className="h-[100vh] gap-3 flex flex-col justify-center items-center  ">
       <img className="w-16" src={imageSrc} alt="" />
       <h1 className="text-lg lg:text-xl font-bold text-zinc-800">{text}</h1>
       <p className="text-zinc-800  text-sm font-medium ">{message}</p>

42d9bb8510be77d0998b98f5f16fc5fd07a0a0a6
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-19 21:05:11 +0600

    dn: with added quickbar toggle in nav & mobile res

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -5,9 +5,26 @@ import { useState } from "react";
 import "../components/Dashboard/Dashboard.css";
 import { QuickBar } from "../components/Dashboard/QuickBar/QuickBar";
 import CurrentPath from "../components/Utils/CurrentPath";
+import { useEffect } from "react";
 const DashboardLayout = () => {
   const [showSidebar, setShowSidebar] = useState(false);
   const [showQuickBar, setShowQuickBar] = useState(false);
+  const toggleSidebarForSmallDevice = () => {
+    if (window.innerWidth < 640) {
+      // Adjust the breakpoint as needed
+      setShowSidebar(true);
+      setShowQuickBar(true);
+    }
+  };
+  useEffect(() => {
+    toggleSidebarForSmallDevice();
+
+    window.addEventListener("resize", toggleSidebarForSmallDevice);
+
+    return () => {
+      window.removeEventListener("resize", toggleSidebarForSmallDevice);
+    };
+  }, []);
   return (
     <div className="flex justify-between transition-transform duration-500 ease-in-out">
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
@@ -18,6 +35,8 @@ const DashboardLayout = () => {
           showSidebar ? "sidebar-open" : "sidebar-closed"
         }`}>
         <DashboardNav
+          showQuickBar={showQuickBar}
+          setShowQuickBar={setShowQuickBar}
           showSidebar={showSidebar}
           setShowSidebar={setShowSidebar}
         />

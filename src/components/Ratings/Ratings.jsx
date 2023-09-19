2bc6354c7a6da4f760ea0998b73db8d77708c9d5
DevReaxul <hreazul11@gmail.com>
2023-09-19 14:54:00 +0600

    fixing bug

--- src/components/Ratings/Ratings.jsx
+++ src/components/Ratings/Ratings.jsx
@@ -1,46 +1,62 @@
-import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+// import { Container, Radio, Rating } from "./RatingStyles";
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Ratings = ({ rate, setRate, data, size }) => {
+  const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 5vh;
-`;
+  font-size: ${size}px;
+  `;
 
-const Radio = styled.input`
+    const Radio = styled.input`
   display: none;
 `;
 
-const Rating = styled.div`
+    const Rating = styled.div`
   cursor: pointer;
+
 `;
+    return (
+        <Container>
+            {[...Array(5)].map((item, index) => {
+                const givenRating = index + 1;
+                return (
+                    <label key={index}>
+                        <Radio
+                            type="radio"
+                            value={givenRating}
+                            onClick={() => {
+                                if (data?.rating) {
+                                    setRate(data?.rating)
+                                }
+                                else {
+                                    setRate(givenRating);
+                                }
 
-const Ratings = ({ rate, setRate, data, size }) => {
+                                // alert(
+                                //     `Are you sure you want to give
+                                //     ${givenRating} stars ?`
+                                // );
+                            }}
+                        />
+                        <Rating>
+                            <FaStar
+                                color={
+                                    givenRating < rate || givenRating === rate
+                                        ? "#FE6244"
+                                        : "rgba(71, 69, 69, 0.168)"
+                                }
+                            />
+                        </Rating>
+                    </label>
+                );
+            })}
+        </Container>
 
-  return (
-    <Container>
-      {[...Array(5)].map((_, index) => {
-        const givenRating = index + 1;
-        return (
-          <label key={index}>
-            <Radio
-              type="radio"
-              value={givenRating}
-            />
-            <Rating>
-              <FaStar
-                color={
-                  givenRating <= rate ? "#FE6244" : "rgba(71, 69, 69, 0.168)"
-                }
-              />
-            </Rating>
-          </label>
-        );
-      })}
-    </Container>
-  );
+    );
 };
 
 export default Ratings;

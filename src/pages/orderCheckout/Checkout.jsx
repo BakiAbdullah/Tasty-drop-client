f48e49bf8b8563a36023c194d8561525a2e64b0c
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-20 00:22:49 +0600

    new bug fixed

--- src/pages/orderCheckout/Checkout.jsx
+++ src/pages/orderCheckout/Checkout.jsx
@@ -76,11 +76,13 @@ export const Checkout = () => {
   }
   console.log(user);
   // handling payment from here
+  const restaurantName = location?.state?.restaurantName;
   const handlePayment = () => {
     const orderInfo = carts.map((cartItem) => {
       const matchingCartItem = carts.find((item) => item._id === cartItem._id);
       const quantity = matchingCartItem ? matchingCartItem.quantity : 0;
       const price = matchingCartItem ? matchingCartItem.menuTotalPrice : 0;
+
       const menuItemName = matchingCartItem
         ? matchingCartItem.menuItemName
         : "";
@@ -91,6 +93,7 @@ export const Checkout = () => {
         quantity: quantity,
         productTotalPrice: price,
         itemName: menuItemName,
+        restaurantName: restaurantName,
       };
 
       return foodItem;
@@ -236,8 +239,7 @@ export const Checkout = () => {
           </div>
           <form
             onSubmit={handleDataUpdate}
-            className="flex flex-col shadow-md space-y-6 bg-white p-7 rounded-xl"
-          >
+            className="flex flex-col shadow-md space-y-6 bg-white p-7 rounded-xl">
             <div>
               <h1 className="div-title">Personal Details</h1>
             </div>
@@ -280,8 +282,7 @@ export const Checkout = () => {
               disabled={isLoading}
               type="submit"
               className={`   bg-orange-500
-              py-2 w-44  text-white font-bold rounded mt-5 px-3 hover:`}
-            >
+              py-2 w-44  text-white font-bold rounded mt-5 px-3 hover:`}>
               {isLoading ? (
                 <FiLoader
                   className="animate-spin m-auto text-white "
@@ -307,32 +308,28 @@ export const Checkout = () => {
                 className={`border border-slate-300 rounded-full p-2 text-xs ${
                   selectedTip === 0 ? "bg-orange-500 text-white" : ""
                 }`}
-                onClick={() => handleTipSelection(0)}
-              >
+                onClick={() => handleTipSelection(0)}>
                 Not Now
               </button>
               <button
                 className={`border border-slate-300 rounded-full p-2 text-xs ${
                   selectedTip === 10 ? "bg-orange-500 text-white" : ""
                 }`}
-                onClick={() => handleTipSelection(10)}
-              >
+                onClick={() => handleTipSelection(10)}>
                 Tk 10
               </button>
               <button
                 className={`border border-slate-300 rounded-full p-2 text-xs ${
                   selectedTip === 30 ? "bg-orange-500 text-white" : ""
                 }`}
-                onClick={() => handleTipSelection(30)}
-              >
+                onClick={() => handleTipSelection(30)}>
                 Tk 30
               </button>
               <button
                 className={`border border-slate-300 rounded-full p-2 text-xs ${
                   selectedTip === 50 ? "bg-orange-500 text-white" : ""
                 }`}
-                onClick={() => handleTipSelection(50)}
-              >
+                onClick={() => handleTipSelection(50)}>
                 Tk 50
               </button>
             </span>

49af26f4dfc7fec8c482651d448e4a5c1d9ecd5a
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-20 14:57:22 +0600

    done with settings and res list dark mode

--- src/components/Dashboard/QuickBarTools/Settings/Settings.jsx
+++ src/components/Dashboard/QuickBarTools/Settings/Settings.jsx
@@ -29,30 +29,32 @@ export const Settings = ({
         isShowSetting ? "visible shadow-bar" : "invisible"
       } bg-white border border-slate-200 p-7 shadow-xl space-y-4 text-lg z-20 h-full lg:w-[350px]  dark-bg `}>
       <span className="flex items-center  justify-between">
-        <h1 className="text-slate-500">Settings</h1>
+        <h1 className="text-slate-500 dark-title">Settings</h1>
         <button onClick={() => setShowSetting(!isShowSetting)}>
           <RxCrossCircled className="hover:text-slate-500 " size={22} />
         </button>
       </span>
       <hr className="border border-slate-200" />
-      <h1 className="lg:text-[15px] font-semibold text-slate-500">Customize</h1>
-      <div className="flex items-center gap-3 text-sm lg:text-[15px] ">
+      <h1 className="lg:text-[15px] font-semibold text-slate-500 dark-title">
+        Customize
+      </h1>
+      <div className="flex items-center gap-3 text-sm lg:text-[15px] dark-text ">
         <DarkMode /> Dark Mode
       </div>
-      <div className="flex items-center gap-3 text-sm lg:text-[15px] ">
+      <div className="flex items-center gap-3 text-sm lg:text-[15px] dark-text">
         <Toggle enabled={showQuickBar} setEnabled={setShowQuickBar} /> Hide
         Quickbar
       </div>
 
-      <h1 className="lg:text-[15px] font-semibold text-slate-500">
+      <h1 className="lg:text-[15px] font-semibold text-slate-500  dark-text">
         Keyboard shortcuts
       </h1>
-      <p className="text-sm">
-        <span className="text-orange-500 font-mono font-semibold">Esc</span>{" "}
+      <p className="text-sm dark-text">
+        <span className="text-orange-500 font-mono font-semibold ">Esc</span>{" "}
         close quickbar
       </p>
-      <p className="text-sm">
-        <span className="text-orange-500 font-mono font-semibold">
+      <p className="text-sm dark-text">
+        <span className="text-orange-500 font-mono font-semibold ">
           Alt + (1 - 6)
         </span>{" "}
         to open quickbar
--- src/components/Utils/Spinner.jsx
+++ src/components/Utils/Spinner.jsx
@@ -3,7 +3,7 @@ import { FiLoader } from "react-icons/fi";
 
 const Spinner = () => {
   return (
-    <div className="min-h-[calc(100vh-200px)]   flex justify-center items-center">
+    <div className="min-h-[calc(100vh-200px)] dark:text-white  flex justify-center items-center">
       <FiLoader className="animate-spin" size={30} />
     </div>
   );

5a9672135bb8bccb230df4379c3da8c12c523021
DevReaxul <hreazul11@gmail.com>
2023-09-21 00:43:39 +0600

    conflict resolved

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -115,7 +115,6 @@ const ManageUsers = () => {
                 value={filterRole}
                 onChange={(e) => setFilterRole(e.target.value)}>
                 <option value="all">All</option>
-                <option value="customer">Customer</option>
                 <option value="admin">Admin</option>
                 <option value="rider">Rider</option>
                 <option value="partner">Partner</option>
